7ef22ee2b6b41c869466e9832efd914936e039a8
Nestor Velez <nvelezv@sura.com.co>
2021-09-14 14:17:47 -0500

    Se soluciona reto planteado en clase

--- lib/ejemplotextfieldyalert/estructura.dart
+++ lib/ejemplotextfieldyalert/estructura.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:appflutter/ejemplotextfieldyalert/textfield.dart';
 
 class VistaInputYAlert extends StatefulWidget {
   @override
@@ -9,6 +8,52 @@ class VistaInputYAlert extends StatefulWidget {
 }
 
 class _VistaInputYAlert extends State<VistaInputYAlert> {
+  TextEditingController controladorInput = TextEditingController();
+  List<Widget> valoresIngresados = [];
+
+  void agregarValorIngresado() {
+    String valorIngresado = controladorInput.text;
+    setState(() {
+      var textoIngresado = Text(valorIngresado);
+      valoresIngresados.add(textoIngresado);
+      controladorInput.text = "";
+      print("habilitado");
+    });
+  }
+
+  void eliminarPalabra() {
+    if (valoresIngresados.isNotEmpty) {
+      setState(() {
+        valoresIngresados.removeLast();
+      });
+    }
+    Navigator.pop(context);
+  }
+
+  void mostrarAlert() {
+    AlertDialog dialog = AlertDialog(
+      content: Text("¿Está seguro que dese eliminar esa palabra?"),
+      actions: [
+        TextButton(
+            onPressed: () {
+              eliminarPalabra();
+            },
+            child: Text("Sí")),
+        TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text("No")),
+      ],
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return dialog;
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +61,33 @@ class _VistaInputYAlert extends State<VistaInputYAlert> {
           title: Text("Ejemplo de TextField y Alert"),
         ),
         body: Container(
-          child: Column(
-            children: [InputText()],
+            child: Column(children: [
+          TextField(
+              controller: controladorInput,
+              decoration: InputDecoration(
+                hintText: "Ingrese un texto",
+                helperText: "El texto digitado será mostrado más abajo",
+                helperStyle: TextStyle(fontSize: 14.0),
+              )),
+          Row(
+            children: [
+              ElevatedButton(
+                  child: Text("Agregar palabra"),
+                  onPressed: () {
+                    agregarValorIngresado();
+                  }),
+              ElevatedButton(
+                  child: Text("Eliminar palabra"),
+                  onPressed: valoresIngresados.isNotEmpty
+                      ? () {
+                          mostrarAlert();
+                        }
+                      : null),
+            ],
           ),
-        ));
+          Column(
+            children: valoresIngresados,
+          ),
+        ])));
   }
 }
